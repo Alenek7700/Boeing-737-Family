f4fca12db455f7cee4525daaf11cf66411a9873d
APDEV052007 <alphapolanddev@gmail.com>
2023-05-14 00:02:50 +0200

    BIG changes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Boeing 737 Family
  Projekt stronki o Geneacjach Boeingów 737. 
  pawełku daddy daj 6 a będe very happy
+ 
--- boeing shop/index.js
+++ boeing shop/index.js
@@ -1,4 +1,6 @@
 if (window.innerWidth < 300) {
     window.location.href = "https://praca.mcdonalds.pl/";
+    //przyszła praca 1ip
+    // bez wyjątków uwu
   }
   
