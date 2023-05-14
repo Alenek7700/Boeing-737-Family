2528cd0a4418605dc9d874a88dd4fcd1e9b6f424
APDEV052007 <alphapolanddev@gmail.com>
2023-05-14 17:00:43 +0200

    hsadajskndjkasdkjnasjkdnkjasdjkabskd

--- boeing shop/index.js
+++ boeing shop/index.js
@@ -4,3 +4,12 @@ if (window.innerWidth < 300) {
     // bez wyjątków uwu
   }
   
+  // film kod
+  function pokazFilm() {
+    document.getElementById("film-okno").style.display = "block";
+  }
+
+  function schowajFilm() {
+    document.getElementById("film-okno").style.display = "none";
+  }
+
